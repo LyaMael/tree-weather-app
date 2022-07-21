d214ca2ab68fb4ef15f75e0688b2d557b7514065
LyaMael <janina.wehner@gmx.net>
2022-07-21 17:50:42 +0200

    Added current date

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-
+function foramtDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "Noveber",
+    "December",
+  ];
+  let day = days[now.getDay()];
+  let month = months[now.getMonth()];
+  let currentDate = now.getDate();
+  let timeHour = now.getHours();
+  if (timeHour < 10) {
+    timeHour = `0${timeHour}`;
+  }
+  let timeMin = now.getMinutes();
+  if (timeMin < 10) {
+    timeMin = `0${timeMin}`;
+  }
+  return `${day} ${timeHour}:${timeMin} (${month} ${currentDate})`;
+}
+let now = new Date();
+let date = document.querySelector("#current-date");
+date.innerHTML = foramtDate(date);
+//
